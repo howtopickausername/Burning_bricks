540b4a9cd7bd2ffbd865c61534e733993872fb5e
Xiaojiu <xueyefeihong@outlook.com>
2017-03-03 17:08:33 +0800

    state更改

--- Burning_bricks/Game/GameObject.h
+++ Burning_bricks/Game/GameObject.h
@@ -1,22 +1,25 @@
 #pragma once
+#include <vector>
 #include <queue>
 #include "State.h"
 
 class cGameObject
 {
 public:
-	cGameObject(cState const& state);
+	cGameObject(cState const& basic_state);
 	~cGameObject();
 	void SendCmd(cCommand const& cmd);
 	void ProcessCmd();
 	void Update();
 	void Swap();
+	//基础状态保存所有通用属性
+	const pState GetCommonState() const;
 	std::shared_ptr<cGameObject> Copy();
 
 private:
 	std::queue<cCommand> CurCmdQ;
 	std::queue<cCommand> BackCmdQ;
-	pState CurState;
-	pState BackState;
+	std::vector<pState> CurState;
+	std::vector<pState> BackState;
 };
 typedef std::shared_ptr<cGameObject> pGameObject;
